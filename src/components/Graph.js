d98215ff1102e03670acd6e76f80d3165e01219b
dpak1999 <deepakkumardash313@gmail.com>
2020-06-04 18:04:41 +0530

    adding the line chart

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,15 +1,43 @@
 import React, { useState, useEffect } from "react";
+import { Bar, Line } from "react-chartjs-2";
+
+import "./styles.css";
 import { getDailyData } from "../api/Mathdro";
 
 const Graph = () => {
-  const [dailyData, setDailyData] = useState({});
+  const [dailyData, setDailyData] = useState([]);
   useEffect(() => {
     const Api = async () => {
       setDailyData(await getDailyData());
     };
+    console.log(dailyData);
     Api();
   });
-  return <h1>The graph will be shown here</h1>;
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true
+          },
+          {
+            data: dailyData(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255,0,0,0.5)",
+            fill: true
+          }
+        ]
+      }}
+    />
+  ) : null;
+
+  return <div>{lineChart}</div>;
 };
 
 export default Graph;
