5126c1ef03c8467655870d305dd4f782bc264161
dpak1999 <deepakkumardash313@gmail.com>
2020-06-04 16:57:29 +0530

    configuring cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,41 @@
 import React from "react";
 
-const Cards = () => {
-  return <h1>Cards showing info will be here</h1>;
+const Cards = props => {
+  console.log(props);
+  return (
+    <div className="ui three stackable cards">
+      <div className="ui card">
+        <div className="content">
+          <div className="header">INFECTED</div>
+          <div className="meta">data</div>
+          <div className="meta">date</div>
+          <div className="description">
+            <p>No of people infected</p>
+          </div>
+        </div>
+      </div>
+      <div className="ui card">
+        <div className="content">
+          <div className="header">RECOVERED</div>
+          <div className="meta">data</div>
+          <div className="meta">date</div>
+          <div className="description">
+            <p>No of people recovered</p>
+          </div>
+        </div>
+      </div>
+      <div className="ui card">
+        <div className="content">
+          <div className="header">DEATHS</div>
+          <div className="meta">data</div>
+          <div className="meta">date</div>
+          <div className="description">
+            <p>No of people died</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
